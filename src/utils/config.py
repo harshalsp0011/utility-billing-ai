3e295263295a31f974baa597786952097507d35a
HARSHAL SANJIV PATIL <harshal.sanjivpatil2000@gmail.com>
2025-10-20 13:37:29 -0400

    file location update

---
+++ src/utils/config.py
@@ -0,0 +1,25 @@
+# src/config.py
+import os
+from dotenv import load_dotenv
+from sqlalchemy.engine.url import URL
+
+load_dotenv()
+
+DB_TYPE = os.getenv("DB_TYPE", "sqlite")
+DB_URL = None
+
+if DB_TYPE == "postgres":
+    DB_URL = URL.create(
+        drivername="postgresql+psycopg2",
+        username=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        database=os.getenv("DB_NAME"),
+    )
+else:
+    DB_PATH = os.getenv("DB_PATH", "data/project.db")
+    DB_URL = f"sqlite:///{DB_PATH}"
+
+print(f"✅ Using database: {DB_URL}")
+ENV = os.getenv("ENV", "dev")
